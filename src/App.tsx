f98a384d8bfb067d1d2b16e7621f9ece8bb9d4f5
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2024-12-03 16:36:56 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="text-3xl font-mono tracking-wider text-green-400">
+          Starknet will melt faces.
+        </p>
+        <p className="text-xl font-mono text-white">
+          Yes!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
